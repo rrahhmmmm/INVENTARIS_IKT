0c3dca7cdcd628b4846f7bf568fccff434bde202
rrahhmmmm <muhammadbaihaqi0990@gmail.com>
2025-12-04 10:14:55 +0700

    rak bak urutan upate lenght 50

--- database/migrations/2025_10_17_042532_create_t_arsip_table.php
+++ database/migrations/2025_10_17_042532_create_t_arsip_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
             $table->string('PERIHAL',100);
             $table->string('TINGKAT_PENGEMBANGAN', 50);
             $table->string('KONDISI', 100)->nullable();
-            $table->string('RAK_BAK_URUTAN',11);
+            $table->string('RAK_BAK_URUTAN',50);
             $table->string('KETERANGAN_SIMPAN', 15);
             $table->string('TIPE_RETENSI', 150)->nullable();
             $table->date('TANGGAL_RETENSI')->nullable();
